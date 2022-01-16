ea23ae957879daac3cb94bd344949ca577c22205
yuito118 <72675978+yuito118@users.noreply.github.com>
2022-01-16 17:35:59 +0900

    holeのサイズ変更

--- Assets/MyScript/HolePlacer.cs
+++ Assets/MyScript/HolePlacer.cs
@@ -10,15 +10,22 @@ public class HolePlacer : MonoBehaviour
     GameObject holes;
     public GameObject holesPrefab;
     ARRaycastManager arRaycastManager;
+    ARPlaneManager arPlaneManager;
     List<ARRaycastHit> hits = new List<ARRaycastHit>();
 
     void Start()
     {
         arRaycastManager = GetComponent<ARRaycastManager>();
+        arPlaneManager = GetComponent<ARPlaneManager>();
     }
 
     void Update()
     {
+        foreach (var plane in arPlaneManager.trackables)
+        {
+            plane.gameObject.SetActive(true);
+        }
+
         if (Input.touchCount > 0)
 	    {
             var touch = Input.GetTouch(0);
@@ -27,12 +34,11 @@ public class HolePlacer : MonoBehaviour
                 // when ray hit plane return true, and plane info in hits
                 if (arRaycastManager.Raycast(touch.position, hits, TrackableType.PlaneWithinPolygon))
                 {
-                    Pose pose = hits[0].pose;
+                    Pose pose = hits[hits.Count - 1].pose;
                     ArriveHole(pose);
 		        }
             }
         }
-	   
     }
 
     void ArriveHole(Pose pose)
