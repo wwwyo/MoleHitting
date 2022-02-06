f9d9a02179e84eb64730bbdae5f63a223d556c59
yuito118 <72675978+yuito118@users.noreply.github.com>
2022-02-06 21:03:37 +0900

    add:function to throw ball

--- Assets/MyScript/BallController.cs
+++ Assets/MyScript/BallController.cs
@@ -13,17 +13,24 @@ public class BallController : MonoBehaviour
     ARRaycastManager arRaycastManager;
     ARPlaneManager arPlaneManager;
     List<ARRaycastHit> hits = new List<ARRaycastHit>();
+    bool isThrow = false;
+    Vector3 startPos;
+    Vector3 endPos;
+    float duration;
+    float xSpeed;
+    float ySpeed;
 
     void Start()
     {
         arCamera = Camera.main;
         Director = GameObject.Find("Director");
         initialScale = transform.localScale;
+        transform.LookAt(arCamera.transform.position);
     }
 
-    // Update is called once per frame
     void Update()
     {
+        duration += Time.deltaTime;
         if (Input.touchCount > 0)
         {
             var touch = Input.GetTouch(0);
@@ -31,6 +38,8 @@ public class BallController : MonoBehaviour
             {
                 case TouchPhase.Began:
                     transform.localScale = (initialScale * 1.2f);
+                    startPos = transform.position;
+                    duration = 0;
                     break;
                 case TouchPhase.Moved:
                     var pos = arCamera.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, 12.5f));
@@ -38,14 +47,22 @@ public class BallController : MonoBehaviour
                     break;
                 case TouchPhase.Ended:
                     transform.localScale = initialScale;
+                    endPos = transform.position;
+                    if (duration > 0.5 && IsFlic())
+                    {
+                        isThrow = true;
+                        ThrowBall();
+                    }
                     break;
                 default:
                     break;
             }
         } else {
+            if (isThrow) {
+                return;
+	        }
             Vector3 pos = arCamera.ViewportToWorldPoint(initialPos);
             transform.position = pos;
-            //transform.LookAt(arCamera.transform);
         }
     }
 
@@ -55,5 +72,20 @@ public class BallController : MonoBehaviour
             Director.GetComponent<Director>().Arrive();
 	    }
     }
+
+    bool IsFlic()
+    {
+        float dist = Vector3.Distance(endPos, startPos);
+        xSpeed = Mathf.Abs(endPos.x - startPos.x) / 500.0f;
+        ySpeed = Mathf.Abs(endPos.y - startPos.y) / 500.0f;
+        Debug.Log(xSpeed.ToString() + ":" + ySpeed.ToString());
+        return (xSpeed >= 3 || ySpeed >= 3);
+    }
+
+    void ThrowBall()
+    {
+        return;
+    }
+
 
 }
